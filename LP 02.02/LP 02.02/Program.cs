b293e6b08535b059cfb266ce94d0a8d0df2d6ef2
kiko124 <109557094+kiko124@users.noreply.github.com>
2025-02-04 18:01:09 +0700

    add dzhka

---
+++ LP 02.02/LP 02.02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LP_02._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            if (100 >= b || b >= 1) { }
+            else
+            {
+                throw new Exception("Число вызло за диапозон!");
+            }
+                if (b % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (b % 5 == 0) {
+            
+                Console.WriteLine("Buzz");
+            }
+            else
+                {
+                    Console.WriteLine(b);
+                }
+
+
+        }
+    }
+}
