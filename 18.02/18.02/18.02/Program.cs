12daa4125f55b46d44e239520bd58deeaedc165e
kiko124 <109557094+kiko124@users.noreply.github.com>
2025-02-11 22:01:52 +0700

    dz

---
+++ 18.02/18.02/18.02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            double[,] b = new double[3, 4];
+            for (int i = 0; i < a.Length; i++)
+            { 
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int item in a)
+                Console.Write($"{item}\t");
+            Console.WriteLine();
+
+            Random random = new Random();
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    b[i,j] = random.NextDouble();
+                }
+                
+
+            }
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    Console.Write($"{b[i, j]}\t");
+                }
+                Console.WriteLine();
+
+            }
+            }
+    }
+}
